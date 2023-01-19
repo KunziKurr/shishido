ecfaa387c716818ef741b41ab293db328ab88747
KunziKurr <korirm3@gmail.com>
2023-01-19 22:42:02 +0100

    Init Bounce

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    Shishido Header Section
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderSection',
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
